3581fa972408cf6125d8c48abf6252e5e89d1481
sagar dhandhalya <dhandhalya.sagar@searce.com>
2022-01-05 17:49:57 +0530

    man and cl.sh added

---
+++ man.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script will use as man script
+# just to check which are the script avavlible
+
+echo cl.sh : remove oracle output files
+echo create.sh : create new shell script file with +x permission
+echo g.sh : git commit push log
+
